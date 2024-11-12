3e8ecb4daa28588cbba21352e4b0d426d69e5a70
Juan Morell Prats <juanmorellprats@gmail.com>
2024-11-12 13:44:20 +0100

    jump more snappy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,9 @@ public class PlayerController : MonoBehaviour
 
     public float rayLength = 1;
     public float jumpStrength = 10.0f;
-    public float jumpSpeedDivisionFactor = 3;
+    float initZAxisPosition;
+    //public float jumpSpeedDivisionFactor = 3;
+    public float gravityAccelerationFactor = 20;
     bool jump = false;
     bool onGround = true;
 
@@ -20,6 +22,7 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        initZAxisPosition = bodyTransform.localPosition.z;
         layerMask = LayerMask.GetMask("Terrain");
         rb = transform.GetChild(0).GetComponent<Rigidbody>();
         print(rb.gameObject.name);
@@ -32,10 +35,23 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
             jump = false;
         }
+
+        
+        if (!onGround 
+            //&& rb.velocity.y <= 0
+            ) 
+        { 
+            //print(rb.velocity.y);
+            rb.AddForce(Vector3.down*100, ForceMode.Acceleration);
+        }
     }
 
     void Update()
     {
+        if (bodyTransform.localPosition.z != initZAxisPosition)
+            bodyTransform.localPosition = new Vector3(bodyTransform.localPosition.x, bodyTransform.localPosition.y, initZAxisPosition);
+
+
         RaycastHit hit;
         Ray ray = new Ray(bodyTransform.position, bodyTransform.TransformDirection(Vector3.down));
 
@@ -46,11 +62,13 @@ public class PlayerController : MonoBehaviour
         //Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.red, 1000f);
         //print(Physics.Raycast(ray, out hit, rayLength, layerMask));
 
-        if (Input.GetAxis("Horizontal") == 0) currentSpeed = 0;
+        //if (Input.GetAxis("Horizontal") == 0) currentSpeed = 0;
 
-        currentSpeed = onGround ? currentSpeed + 0.03f : currentSpeed - 0.06f;
-        currentSpeed = Mathf.Clamp(currentSpeed, speed / jumpSpeedDivisionFactor, speed);
+        //currentSpeed = onGround ? currentSpeed + 0.03f : currentSpeed - 0.06f;
+        //currentSpeed = Mathf.Clamp(currentSpeed, speed / jumpSpeedDivisionFactor, speed);
 
-        transform.Rotate(new Vector3(0, -currentSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0));
+        transform.Rotate(new Vector3(0, -speed * Time.deltaTime * Input.GetAxis("Horizontal"), 0));
+        //transform.Rotate(new Vector3(0, -currentSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0));
+
     }
 }

1b5e0b18e7aa1a4c04721e9f1ad9eedc8bdca1d4
JuanMorell <juanmorellprats@gmail.com>
2024-11-13 09:34:36 +0100

    controlado WIP

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,51 +6,53 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 6.0f;
-    float currentSpeed = 0;
+    float horizontal;
 
     public float rayLength = 1;
     public float jumpStrength = 10.0f;
     float initZAxisPosition;
-    //public float jumpSpeedDivisionFactor = 3;
+    public float jumpSpeedDivisionFactor = 4;
     public float gravityAccelerationFactor = 20;
     bool jump = false;
     bool onGround = true;
+    bool againstWall = false;
 
     LayerMask layerMask;
     public Transform bodyTransform;
+    public Transform centralTransform;
     Rigidbody rb;
 
     void Start()
     {
         initZAxisPosition = bodyTransform.localPosition.z;
         layerMask = LayerMask.GetMask("Terrain");
-        rb = transform.GetChild(0).GetComponent<Rigidbody>();
-        print(rb.gameObject.name);
+        rb = GetComponent<Rigidbody>();
+        //rb = transform.GetChild(0).GetComponent<Rigidbody>();
+        //print(rb.gameObject.name);
     }
 
     void FixedUpdate()
     {
         if (jump)
         {
-            rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * jumpStrength * 100, ForceMode.Impulse);
             jump = false;
         }
 
         
-        if (!onGround 
-            //&& rb.velocity.y <= 0
-            ) 
-        { 
-            //print(rb.velocity.y);
-            rb.AddForce(Vector3.down*100, ForceMode.Acceleration);
-        }
+        if (!onGround) 
+            rb.AddForce(Vector3.down * (jumpStrength / jumpSpeedDivisionFactor) * 100, ForceMode.Acceleration);
+        
+
+        rb.AddForce(speed * horizontal * bodyTransform.TransformDirection(Vector3.right), ForceMode.VelocityChange);
     }
 
     void Update()
     {
+        horizontal = Input.GetAxis("Horizontal");
+
         if (bodyTransform.localPosition.z != initZAxisPosition)
             bodyTransform.localPosition = new Vector3(bodyTransform.localPosition.x, bodyTransform.localPosition.y, initZAxisPosition);
-
 
         RaycastHit hit;
         Ray ray = new Ray(bodyTransform.position, bodyTransform.TransformDirection(Vector3.down));
@@ -59,16 +61,16 @@ public class PlayerController : MonoBehaviour
 
         if (onGround && Input.GetKeyDown(KeyCode.Space) && !jump) jump = true;
 
-        //Debug.DrawRay(ray.origin, ray.direction * rayLength, Color.red, 1000f);
-        //print(Physics.Raycast(ray, out hit, rayLength, layerMask));
-
-        //if (Input.GetAxis("Horizontal") == 0) currentSpeed = 0;
-
-        //currentSpeed = onGround ? currentSpeed + 0.03f : currentSpeed - 0.06f;
-        //currentSpeed = Mathf.Clamp(currentSpeed, speed / jumpSpeedDivisionFactor, speed);
-
-        transform.Rotate(new Vector3(0, -speed * Time.deltaTime * Input.GetAxis("Horizontal"), 0));
-        //transform.Rotate(new Vector3(0, -currentSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), 0));
-
+        bodyTransform.LookAt(new Vector3(250, bodyTransform.position.y, 250));
     }
+
+    //void OnCollisionStay(Collision other)
+    //{
+    //    onGround = other.gameObject.CompareTag("Terrain");
+    //}
+
+    //void OnCollisionExit(Collision collision)
+    //{
+    //    onGround = !onGround;
+    //}
 }
